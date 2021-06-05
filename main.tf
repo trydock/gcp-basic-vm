d0424b7f94824a8d685bca74681b354866b986c2
debuggerboy <debuggerboy@gmail.com>
2021-06-06 05:25:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ provider "google" {
 module "vm_instance_template" {
   source               = "terraform-google-modules/vm/google//modules/instance_template"
   version              = "6.4.0"
-  name_prefix          = "instance-template-"
-  machine_type         = "f1-micro"
-  region               = "asia-southeast1"
-  source_image_project = "debian-cloud"
-  source_image_family  = "debian-10"
-  subnetwork           = "default"
+  name_prefix          = var.gcp_instance_template_prefix
+  machine_type         = var.gcp_instance_template_machine_type
+  region               = var.gcp_region
+  source_image_project = var.gcp_instance_template_image_project
+  source_image_family  = var.gcp_instance_template_image_family
+  subnetwork           = var.gcp_subnetwork
   service_account      = var.service_account
   metadata = {
     ssh-keys = "anish:${file("~/.ssh/id_rsa.pub")}"
@@ -23,7 +23,7 @@ module "vm_compute_instance" {
   source            = "terraform-google-modules/vm/google//modules/compute_instance"
   version           = "6.4.0"
   instance_template = module.vm_instance_template.self_link
-  subnetwork        = "default"
+  subnetwork        = var.gcp_subnetwork
   access_config = [{
     nat_ip       = var.nat_ip
     network_tier = var.network_tier
